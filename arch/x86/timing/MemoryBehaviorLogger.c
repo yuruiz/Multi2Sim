86f1afae8b46c2ea994f7f3e313b03be4b96982b
witan <accountregistor@gmail.com>
2013-11-21 10:13:20 +0800

    Add MemoryDrivenPrefetcher without debug

--- arch/x86/timing/MemoryBehaviorLogger.c
+++ arch/x86/timing/MemoryBehaviorLogger.c
@@ -4,85 +4,90 @@
 
 void x86ThreadInitMemoryBehaviorLogger(X86Thread *self)
 {
-	// if (!self->memlogger)
-	// {
-	// 	return;
-	// }
+    // if (!self->memlogger)
+    // {
+    //  return;
+    // }
 
-	// self->memlogger = (struct x86_mem_behavr_logger_t *) xcalloc(1, sizeof(struct x86_mem_behavr_logger_t));
+    // self->memlogger = (struct x86_mem_behavr_logger_t *) xcalloc(1, sizeof(struct x86_mem_behavr_logger_t));
 }
 
 void x86ThreadFreeMemoryBehaviorLogger(X86Thread *self)
 {
-	// free(self->memlogger);
+    // free(self->memlogger);
 }
 
 /*Yurui Insert stride pattern into Memory behavior logger*/
 void X86InsertInMBL(X86Thread *self, unsigned int address)
 {
-	struct x86_mem_behavr_logger_t *mem_behav_log = &self->memlogger;
-	struct x86_mem_behavr_pattern_t *stride_pattern_logger = mem_behav_log->stride_pattern_log;
-	struct x86_mem_behavr_buffer *buffer = mem_behav_log->buffer;
+    struct x86_mem_behavr_logger_t *mem_behav_log = &self->memlogger;
+    struct x86_mem_behavr_pattern_t *stride_pattern_logger = mem_behav_log->stride_pattern_log;
+    struct x86_mem_behavr_buffer *buffer = mem_behav_log->buffer;
 
-	int index = (address >> ADDRESS_INDEX_SHIFT) % BUFFER_INDEX_SIZE;
-	int addressCount = buffer[index].Count;
+    int index = (address >> ADDRESS_INDEX_SHIFT) % BUFFER_INDEX_SIZE;
+    int addressCount = buffer[index].Count;
 
-	// FILE * pfile;
+    // FILE * pfile;
 
-	buffer[index].Count = (buffer[index].Count + 1) % BUFFER_LENGTH;
+    buffer[index].Count = (buffer[index].Count + 1) % BUFFER_LENGTH;
 
-	assert(Count < BUFFER_LENGTH);
+    assert(Count < BUFFER_LENGTH);
 
-	// pfile = fopen("/home/witan/multi2sim-4.2/samples/x86/Trace.txt", "a");
+    // pfile = fopen("/home/witan/multi2sim-4.2/samples/x86/Trace.txt", "a");
 
-	buffer[index].address[addressCount]= address;
+    buffer[index].address[addressCount] = address;
 
-	// fprintf(stderr, "Insert Address %d into buffer index %d\n", address, index);
+    // fprintf(stderr, "Insert Address %d into buffer index %d\n", address, index);
 
-	if (addressCount != BUFFER_LENGTH - 1)
-	{
-		return;
-	}
+    if (addressCount != BUFFER_LENGTH - 1)
+    {
+        return;
+    }
 
-	long temp_difference = 0;
-	long difference = 0;
+    long temp_difference = 0;
+    long difference = 0;
 
-	int stride_pattern_count = 1;
-	int stride_pattern_end = 0;
-	int stride_pattern_max_length = 2;
+    int stride_pattern_count = 1;
+    int stride_pattern_end = 0;
+    int stride_pattern_max_length = 2;
 
 
-	/*locate longest stride pattern within buffer*/
-	for (int i = 0; i < BUFFER_INDEX_SIZE-2; ++i)
-	{
-		temp_difference = buffer[index].address[i+1] - buffer[index].address[i];
-		if (difference != temp_difference)
-		{
-			difference = temp_difference;
-			stride_pattern_count = 1;
-		}
-		else
-		{
-			stride_pattern_count++;
-			if (stride_pattern_count > stride_pattern_max_length)
-			{
-				stride_pattern_max_length = stride_pattern_count;
-				stride_pattern_end = i+1;
-			}
-		}
-	}
+    /*locate longest stride pattern within buffer*/
+    for (int i = 0; i < BUFFER_INDEX_SIZE - 2; ++i)
+    {
+        temp_difference = buffer[index].address[i + 1] - buffer[index].address[i];
+        if (difference != temp_difference)
+        {
+            difference = temp_difference;
+            stride_pattern_count = 1;
+        }
+        else
+        {
+            stride_pattern_count++;
+            if (stride_pattern_count > stride_pattern_max_length)
+            {
+                stride_pattern_max_length = stride_pattern_count;
+                stride_pattern_end = i + 1;
+            }
+        }
+    }
 
-	/*if address matches stride pattern, add the address into stride pattern logger*/
-	if (stride_pattern_end != 0)
-	{
-		stride_pattern_logger[index].stride = difference;
-		stride_pattern_logger[index].InitialAddress = buffer[index].address[stride_pattern_end - stride_pattern_max_length];
-		stride_pattern_logger[index].instruction_address_count = stride_pattern_max_length;
+    int maskindex = (int) (index / 8);
+    int maxkbit   = index % 8;
+    /*if address matches stride pattern, add the address into stride pattern logger*/
+    if (stride_pattern_end != 0)
+    {
+        stride_pattern_logger[index].stride = difference;
+        stride_pattern_logger[index].InitialAddress = buffer[index].address[stride_pattern_end - stride_pattern_max_length];
+        stride_pattern_logger[index].instruction_address_count = stride_pattern_max_length;
+        
+        /*Mask Bit the Summary*/
+        mem_behav_log->StridePatternMask[maskindex] = mem_behav_log->StridePatternMask[maskindex] | (1 << maxkbit);
 
-		fprintf(stderr, "Stride pattern with index %d update with stride %d, InitialAddress %d and length %d \n",
-			index, stride_pattern_logger[index].stride, stride_pattern_logger[index].InitialAddress,
-			stride_pattern_logger[index].instruction_address_count);
+        fprintf(stderr, "Stride pattern with index %d update with stride %d, InitialAddress %d and length %d \n",
+                index, stride_pattern_logger[index].stride, stride_pattern_logger[index].InitialAddress,
+                stride_pattern_logger[index].instruction_address_count);
 
-	}
-	// fclose(pfile);
+    }
+    // fclose(pfile);
 }
