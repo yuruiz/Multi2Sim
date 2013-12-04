11872910359cdc47836c553d84c7ca4c6af7b862
witan <accountregistor@gmail.com>
2013-12-04 10:27:17 +0800

    Add MRU pattern

--- arch/x86/timing/MemoryBehaviorLogger.c
+++ arch/x86/timing/MemoryBehaviorLogger.c
@@ -4,12 +4,23 @@
 
 void x86ThreadInitMemoryBehaviorLogger(X86Thread *self)
 {
+	struct x86_MRU_pattern_t *mru_i_pattern_logger = mem_behav_log->MRU_Instruction_log;
 	// if (!self->memlogger)
 	// {
 	// 	return;
 	// }
 
 	// self->memlogger = (struct x86_mem_behavr_logger_t *) xcalloc(1, sizeof(struct x86_mem_behavr_logger_t));
+
+	for (int index = 0; index < MAX_PATTERN_COUNT; ++index)
+	{
+		mru_i_pattern_logger[index].contex_id = 0;
+		for (int way = 0; way < MRU_ASSOCIATIVITY; ++way)
+		{
+			mru_i_pattern_logger[index].tag[way] = 0;
+			mru_i_pattern_logger[index].counter[way] = way;
+		}
+	}
 }
 
 void x86ThreadFreeMemoryBehaviorLogger(X86Thread *self)
@@ -18,71 +29,115 @@ void x86ThreadFreeMemoryBehaviorLogger(X86Thread *self)
 }
 
 /*Yurui Insert stride pattern into Memory behavior logger*/
-void X86InsertInMBL(X86Thread *self, unsigned int address)
+void X86InsertInMBL(X86Thread *self, unsigned int address, Patterns pattern)
 {
 	struct x86_mem_behavr_logger_t *mem_behav_log = &self->memlogger;
-	struct x86_mem_behavr_pattern_t *stride_pattern_logger = mem_behav_log->stride_pattern_log;
-	struct x86_mem_behavr_buffer *buffer = mem_behav_log->buffer;
 
 	int index = (address >> ADDRESS_INDEX_SHIFT) % BUFFER_INDEX_SIZE;
-	int addressCount = buffer[index].Count;
 
-	// FILE * pfile;
-
-	buffer[index].Count = (buffer[index].Count + 1) % BUFFER_LENGTH;
-
-	assert(Count < BUFFER_LENGTH);
-
-	// pfile = fopen("/home/witan/multi2sim-4.2/samples/x86/Trace.txt", "a");
-
-	buffer[index].address[addressCount]= address;
-
-	// fprintf(stderr, "Insert Address %d into buffer index %d\n", address, index);
-
-	if (addressCount != BUFFER_LENGTH - 1)
+	switch(pattern):
 	{
-		return;
-	}
-
-	long temp_difference = 0;
-	long difference = 0;
-
-	int stride_pattern_count = 1;
-	int stride_pattern_end = 0;
-	int stride_pattern_max_length = 2;
-
-
-	/*locate longest stride pattern within buffer*/
-	for (int i = 0; i < BUFFER_INDEX_SIZE-2; ++i)
-	{
-		temp_difference = buffer[index].address[i+1] - buffer[index].address[i];
-		if (difference != temp_difference)
+		case Stride_Pattern:
 		{
-			difference = temp_difference;
-			stride_pattern_count = 1;
-		}
-		else
-		{
-			stride_pattern_count++;
-			if (stride_pattern_count > stride_pattern_max_length)
+			struct x86_stride_pattern_t *stride_pattern_logger = mem_behav_log->stride_pattern_log;
+			struct x86_mem_behavr_buffer *buffer = mem_behav_log->buffer;
+			int addressCount = buffer[index].Count;
+
+			// FILE * pfile;
+
+			buffer[index].Count = (buffer[index].Count + 1) % BUFFER_LENGTH;
+
+			// assert(Count < BUFFER_LENGTH);
+
+			// pfile = fopen("/home/witan/multi2sim-4.2/samples/x86/Trace.txt", "a");
+
+			buffer[index].address[addressCount]= address;
+
+			// fprintf(stderr, "Insert Address %d into buffer index %d\n", address, index);
+
+			if (addressCount != BUFFER_LENGTH - 1)
 			{
-				stride_pattern_max_length = stride_pattern_count;
-				stride_pattern_end = i+1;
+				return;
+			}
+
+			long temp_difference = 0;
+			long difference = 0;
+
+			int stride_pattern_count = 1;
+			int stride_pattern_end = 0;
+			int stride_pattern_max_length = 2;
+
+
+			/*locate longest stride pattern within buffer*/
+			for (int i = 0; i < BUFFER_INDEX_SIZE-2; ++i)
+			{
+				temp_difference = buffer[index].address[i+1] - buffer[index].address[i];
+				if (difference != temp_difference)
+				{
+					difference = temp_difference;
+					stride_pattern_count = 1;
+				}
+				else
+				{
+					stride_pattern_count++;
+					if (stride_pattern_count > stride_pattern_max_length)
+					{
+						stride_pattern_max_length = stride_pattern_count;
+						stride_pattern_end = i+1;
+					}
+				}
+			}
+
+			/*if address matches stride pattern, add the address into stride pattern logger*/
+			if (stride_pattern_end != 0)
+			{
+				stride_pattern_logger[index].stride = difference;
+				stride_pattern_logger[index].InitialAddress = buffer[index].address[stride_pattern_end - stride_pattern_max_length];
+				stride_pattern_logger[index].instruction_address_count = stride_pattern_max_length;
+
+				fprintf(stderr, "Stride pattern with index %d update with stride %d, InitialAddress %d and length %d \n",
+					index, stride_pattern_logger[index].stride, stride_pattern_logger[index].InitialAddress,
+					stride_pattern_logger[index].instruction_address_count);
+
+			}
+			break;
+		}
+
+		case: MRU_Instructioin
+		{
+			struct x86_MRU_pattern_t *mru_i_pattern_logger = mem_behav_log->MRU_Instruction_log;
+			int Found_way = -1;
+
+			for (int way = 0; way < MRU_ASSOCIATIVITY; ++way)
+			{
+				/*Invalid Way Found*/
+				if(!mru_i_pattern_logger[index].tag[way])
+				{
+					mru_i_pattern_logger[index].tag[way] = address;
+					break;
+				}
+
+				/*hit*/
+				if ((mru_i_pattern_logger[index].tag[way] >> ADDRESS_INDEX_SHIFT) == (address >> ADDRESS_INDEX_SHIFT))
+				{
+					break;
+				}
+			}
+
+			if (Found_way == -1)
+			{
+				for (int way = 0; way < MRU_ASSOCIATIVITY; ++way)
+				{
+					mru_i_pattern_logger[index].counter[way]--;
+
+					if (mru_i_pattern_logger[index].counter[way] < 0)
+					{
+						mru_i_pattern_logger[index].counter[way] = MRU_ASSOCIATIVITY - 1;
+						mru_i_pattern_logger[index].tag[way] = address;
+					}
+				}
 			}
 		}
-	}
-
-	/*if address matches stride pattern, add the address into stride pattern logger*/
-	if (stride_pattern_end != 0)
-	{
-		stride_pattern_logger[index].stride = difference;
-		stride_pattern_logger[index].InitialAddress = buffer[index].address[stride_pattern_end - stride_pattern_max_length];
-		stride_pattern_logger[index].instruction_address_count = stride_pattern_max_length;
-
-		fprintf(stderr, "Stride pattern with index %d update with stride %d, InitialAddress %d and length %d \n",
-			index, stride_pattern_logger[index].stride, stride_pattern_logger[index].InitialAddress,
-			stride_pattern_logger[index].instruction_address_count);
-
 	}
 	// fclose(pfile);
 }
--- arch/x86/timing/MemoryBehaviorLogger.h
+++ arch/x86/timing/MemoryBehaviorLogger.h
@@ -3,22 +3,44 @@
 
 #include "thread.h"
 
+#define MAX_PATTERN_COUNT 256
+#define MRU_ASSOCIATIVITY 4;
+
+/*To hide the address offset*/
+#define ADDRESS_INDEX_SHIFT       8
+// #define PATTERN_RECORD_THRESHOULD 2
+#define BUFFER_INDEX_SIZE 64
+#define BUFFER_LENGTH     32
+// #define MAX_INSTRUCTION_ADDRESS_COUNT 1000
+
+typedef enum
+{
+	Stride_Pattern;
+	MRU_Instructioin;
+	MRU_DATA;
+ }Patterns;
+
 /*Yurui Memory Behavior Logger Pattern*/
-struct x86_mem_behavr_pattern_t
+struct x86_stride_pattern_t
 {
 	/*Recorded Instruction Counter*/
+	int context_id;
 	int instruction_address_count;
 	int stride;
 	unsigned int InitialAddress;
 
-#define MAX_INSTRUCTION_ADDRESS_COUNT 1000
-	unsigned int address[MAX_INSTRUCTION_ADDRESS_COUNT];
+	// unsigned int address[MAX_INSTRUCTION_ADDRESS_COUNT];
 };
 
-#define ADDRESS_INDEX_SHIFT       8
-#define PATTERN_RECORD_THRESHOULD 2
-#define BUFFER_INDEX_SIZE 64
-#define BUFFER_LENGTH     32
+struct x86_MRU_pattern_t
+{
+	int context_id;
+
+	/*Initial Address of the block*/
+	unsigned int tag[MRU_ASSOCIATIVITY];
+
+	unsigned int counter[MRU_ASSOCIATIVITY];
+};
 
 struct x86_mem_behavr_buffer
 {
@@ -26,17 +48,16 @@ struct x86_mem_behavr_buffer
 	unsigned int address[BUFFER_LENGTH];
 };
 
+
 struct x86_mem_behavr_logger_t
 {
 	/*other Pattern to add*/
 
 	struct x86_mem_behavr_buffer buffer[BUFFER_INDEX_SIZE];
 
-
-#define MAX_PATTERN_COUNT 64
-
-	struct x86_mem_behavr_pattern_t stride_pattern_log[MAX_PATTERN_COUNT];
-	struct x86_mem_behavr_pattern_t ptrchase_pattern_log[MAX_PATTERN_COUNT];
+	struct x86_stride_pattern_t stride_pattern_log[MAX_PATTERN_COUNT];
+	struct x86_MRU_pattern_t MRU_Instruction_log[MAX_PATTERN_COUNT];
+	struct x86_MRU_pattern_t MRU_DATA_log[MAX_PATTERN_COUNT];
 	/*other Pattern to add*/
 
 	/*Pattern Mask*/
@@ -51,6 +72,6 @@ void x86ThreadInitMemoryBehaviorLogger(X86Thread *self);
 
 void x86ThreadFreeMemoryBehaviorLogger(X86Thread *self);
 
-void X86InsertInMBL(X86Thread *self, unsigned int address);
+void X86InsertInMBL(X86Thread *self, unsigned int address, Patterns pattern);
 
 #endif
